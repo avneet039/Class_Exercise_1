f4f0f321f34da06d9a78e49c9a53aa3d256696e3
tworkmay <104872846+tworkmay@users.noreply.github.com>
2022-05-03 19:53:22 -0400

    Add files via upload

---
+++ Class_Exercise_1.py
@@ -0,0 +1,26 @@
+n1 = input("Enter first number:")
+n2 = input("Enter second number:")
+
+#Subtraction 
+s = float(n1) - float(n2)
+print("The result of the subtraction is: ", s)
+
+
+#Addition
+a = float(n1) + float(n2)
+print("The result of the addition is: ", a)
+
+#Multiplication
+m = float(n1) * float(n2)
+print("The result of the multiplication is: ", m)
+
+#Power
+p = float(n1) ** float(n2)
+print("The result of the power operation is: ", p)
+
+#Division
+if float(n2) == 0:
+    print("Invalid Input for division")
+else:
+    d = float(n1) / float(n2)
+    print("The result of the division is: ", d)
