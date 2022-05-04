fd020ccf8e9beb8ee0d235ad3b0b383dc3cc20c1
avneet039 <104873144+avneet039@users.noreply.github.com>
2022-05-03 20:07:49 -0400

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,39 @@
+val1 = float(input("Enter first number:"))
+val2 = float(input("Enter second number:"))
+val3= float(input("Enter third number:"))
+#Subtaction 
+sub = (val1-val2) *val3
+print("The result of the subtraction is: ", sub)
+
+
+#Addition
+add = val1+val2+val3
+print("The result of the addition is: ", add)
+
+#Multiplication
+mul = val1 * val2*val3
+print("The result of the multiplication is: ", mul)
+
+#Power
+pow = (val1**val2)+val3
+print("The result of the power operation is: ", pow)
+
+#Division
+if float(val2) == 0:
+    print("Invalid Input for division")
+else:
+    div = (val1 / val2)+val3
+    print("The result of the division is: ", div)
+
+
+
+int1 = float(input("Enter your value:"))
+int2 = float(input("Enter the value:" ))
+print(int1)
+print(int2)
+
+print(int2 - int1)
+print(int1+int2)
+print(int1*int2)
+print(int1**int2)
+print(int1/int2)
